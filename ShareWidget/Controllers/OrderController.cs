0b76e32d2de1b50af3e585658fd725635c68eeac
Peter Samuel <pjsamuel3@hotmail.com>
2012-03-20 23:14:48 +0100

    Added Solution and nuget packages

---
+++ ShareWidget/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShareWidget.Models;
+
+namespace ShareWidget.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult Index(string id)
+        {
+            var viewModel = new OrderViewModel {Id = id};
+            return View(viewModel);
+        }
+    }
+}
---
+++ ShareWidget/Models/OrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShareWidget.Models
+{
+    public class OrderViewModel
+    {
+        public string Id { get; set; }
+    }
+}
