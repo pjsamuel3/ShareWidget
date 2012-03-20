622b493d7477adf9fffa3ac6c2a8b42fad5c4933
Peter Samuel <pjsamuel3@hotmail.com>
2012-03-20 23:50:33 +0100

    Added viewModel test data

--- ShareWidget/Scripts/KnockoutJS/shareViewModel.js
+++ ShareWidget/Scripts/KnockoutJS/shareViewModel.js
@@ -1,2 +1,12 @@
 ﻿var shareViewModel = shareViewModel || {};
 
+var shareViewModel = {
+    orderid: ko.observable("1234"),
+    lastName: ko.observable("Smith"),
+    products: ko.observableArray(["Cat", "Dog", "Fish"]),
+    type: "Share"
+};
+
+shareViewModel.hasALotOfProducts = ko.computed(function () {
+    return this.products().length > 2;  
+}, shareViewModel)
