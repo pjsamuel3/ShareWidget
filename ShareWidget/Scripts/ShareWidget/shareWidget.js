5f7ee7262c72c70d8664b2c94d17f27b7afb9121
Peter Samuel <pjsamuel3@hotmail.com>
2012-04-10 19:11:15 +0200

    Tightened UI and removed slider

--- ShareWidget/Scripts/ShareWidget/shareWidget.js
+++ ShareWidget/Scripts/ShareWidget/shareWidget.js
@@ -26,6 +26,14 @@ $(document).on("click", "a.product-selector", function(e) {
     shareViewModel.SelectedProduct(clickedProduct);
 });
 
+$(document).on("click", ".next", function (e) {
+    shareWidget.next();
+});
+
+$(document).on("click", ".previous", function (e) {
+    shareWidget.previous();
+});
+
 $(document).ready(function() {
     $("#social_choices_tabs").tabs();
     shareWidget.getOrderedProducts("456789");
@@ -35,45 +43,58 @@ shareWidget.setupImageFlip = function () {
 
     shareWidget.productFlip = jQuery('#share_products').jcoverflip({
         current: 1,
-        beforeCss: function (el, container, offset) {
-            return [
-                $.jcoverflip.animationElement(el, { left: (container.width() / 2 - 220 - 110 * offset + 20 * offset) + 'px', bottom: '20px' }, {}),
-                $.jcoverflip.animationElement(el.find('img'), { width: Math.max(10, 100 - 20 * offset * offset) + 'px', opacity: 0.5 }, {})
-            ];
-        },
-        afterCss: function (el, container, offset) {
-            return [
-                $.jcoverflip.animationElement(el, { left: (container.width() / 2 + 10 + 110 * offset) + 'px', bottom: '20px' }, {}),
-                $.jcoverflip.animationElement(el.find('img'), { width: Math.max(10, 100 - 20 * offset * offset) + 'px', opacity: 0.5 }, {})
-            ];
-        },
-        currentCss: function (el, container) {
-            return [
-                $.jcoverflip.animationElement(el, { left: (container.width() / 2 - 100) + 'px', bottom: 0 }, {}),
-                $.jcoverflip.animationElement(el.find('img'), { width: '110px', opacity: 1 }, {})
-            ];
-        },
         change: function (event, ui) {
-            jQuery('#scrollbar').slider('value', ui.to * 25);
             var currentItem = $('#share_products').jcoverflip('current');
-            console.log(currentItem);
             var clickedProduct = $($('#share_products > li')[currentItem]).find("a").attr("href").replace("#", "");
             shareViewModel.SelectedProduct(clickedProduct);
         }
     });
 
-    jQuery('#scrollbar').slider({
-        value: 50,
-        stop: function (event, ui) {
-            if (event.originalEvent) {
-                var newVal = Math.round(ui.value / 25);
-                jQuery('#share_products').jcoverflip('current', newVal);
-                jQuery('#scrollbar').slider('value', newVal * 25);
-            }
-        }
-    });
+    //    shareWidget.productFlip = jQuery('#share_products').jcoverflip({
+    //        current: 1,
+    //        beforeCss: function (el, container, offset) {
+    //            return [
+    //                $.jcoverflip.animationElement(el, { left: (container.width() / 2 - 220 - 110 * offset + 20 * offset) + 'px', bottom: '20px' }, {}),
+    //                $.jcoverflip.animationElement(el.find('img'), { width: Math.max(10, 100 - 20 * offset * offset) + 'px', opacity: 0.5 }, {})
+    //            ];
+    //        },
+    //        afterCss: function (el, container, offset) {
+    //            return [
+    //                $.jcoverflip.animationElement(el, { left: (container.width() / 2 + 10 + 110 * offset) + 'px', bottom: '20px' }, {}),
+    //                $.jcoverflip.animationElement(el.find('img'), { width: Math.max(10, 100 - 20 * offset * offset) + 'px', opacity: 0.5 }, {})
+    //            ];
+    //        },
+    //        currentCss: function (el, container) {
+    //            return [
+    //                $.jcoverflip.animationElement(el, { left: (container.width() / 2 - 100) + 'px', bottom: 0 }, {}),
+    //                $.jcoverflip.animationElement(el.find('img'), { width: '110px', opacity: 1 }, {})
+    //            ];
+    //        },
+    //        change: function (event, ui) {
+    //            jQuery('#scrollbar').slider('value', ui.to * 25);
+    //            var currentItem = $('#share_products').jcoverflip('current');
+    //            console.log(currentItem);
+    //            var clickedProduct = $($('#share_products > li')[currentItem]).find("a").attr("href").replace("#", "");
+    //            shareViewModel.SelectedProduct(clickedProduct);
+    //        }
+    //    });
+
+    //    jQuery('#scrollbar').slider({
+    //        value: 50,
+    //        stop: function (event, ui) {
+    //            if (event.originalEvent) {
+    //                var newVal = Math.round(ui.value / 25);
+    //                jQuery('#share_products').jcoverflip('current', newVal);
+    //                jQuery('#scrollbar').slider('value', newVal * 25);
+    //            }
+    //        }
+    //    });
 };
 
 shareWidget.next = function () {
     shareWidget.productFlip.jcoverflip('next');
+};
+
+shareWidget.previous = function () {
+    shareWidget.productFlip.jcoverflip('previous');
 };
