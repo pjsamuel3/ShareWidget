cdd7cadd428886a810bb6be94ffbd869fe9e41c7
Peter Samuel <pjsamuel3@hotmail.com>
2012-05-01 20:56:52 +0200

    Marianne's style changes with rounded edges.

--- ShareWidget/Scripts/ShareWidget/shareWidget.js
+++ ShareWidget/Scripts/ShareWidget/shareWidget.js
@@ -55,7 +55,7 @@ shareWidget.setupImageFlip = function () {
     function customTitleCreate(itemElem) {
         
         var title = $('<div class="title"></div>')
-            .append(itemElem.find('img').attr('alt'))
+            //.append(itemElem.find('img').attr('alt'))
             .append('<a name="fb_share" type="button" share_url="' + itemElem.find('span.product-url').hide().text() + '">Share on facebook</a>')
             .append('<a href="https://twitter.com/share" class="twitter-share-button" data-count="none" data-lang="en" data-url="' + itemElem.find('span.encoded-url').hide().text() + '" data-text="' + itemElem.find('span.tweet-content').hide().text() + '">Tweet</a>');
         return title;
@@ -73,13 +73,34 @@ shareWidget.setupImageFlip = function () {
             shareViewModel.SelectedProduct(clickedProduct);
         },
         titles: { create: customTitleCreate, destroy: customTitleDestroy },
-         currentCss: function (el, container) {
+        beforeCss: function (el, container, offset) {
                 return [
-                    $.jcoverflip.animationElement(el, { left: (container.width() / 2 - 100) + 'px', top: 0 }, {}),
-                    $.jcoverflip.animationElement(el.find('img'), { width: '100px', opacity: 1 }, {})
+                    $.jcoverflip.animationElement(el, { left: (container.width() / 2 - 160 - 110 * offset) + 'px', top: '0' }, {}),
+                    $.jcoverflip.animationElement(el.find('img'), { width: '80px', height: '80px', opacity: 0.4 }, {}),
+                    $.jcoverflip.animationElement(el.find('span.title'), { display: 'none' }, {})
                 ];
-            }
+            },
+        afterCss: function (el, container, offset) {
+                return [
+                    $.jcoverflip.animationElement(el, { left: (container.width() / 2 + 75 + 105 * offset) + 'px', top: '0' }, {}),
+                    $.jcoverflip.animationElement(el.find('img'), { width: '80px', height: '80px', opacity: 0.4 }, {}),
+                    $.jcoverflip.animationElement(el.find('span.title'), { display: 'none' }, {})
+                ];
+            },
+        currentCss: function (el, container) {
+                return [
+				$.jcoverflip.animationElement(el, { left: (container.width() / 2 - 73) + 'px', top: '0' }, {}),
+				$.jcoverflip.animationElement(el.find('img'), { opacity: 1, width: '140px', height: '140px' }, {}),
+                $.jcoverflip.animationElement(el.find('span.title'), { display: 'block' }, {})
+			];
+        }
     });
+
+        //move share buttons out of ul and into separate div
+        $("div.ui-jcoverflip--title").appendTo("#share-buttons");
+
+
+
 
     //    shareWidget.productFlip = jQuery('#share_products').jcoverflip({
     //        current: 1,
